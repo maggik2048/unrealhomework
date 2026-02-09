70e3fc1c4f89008bbb534e9f6b5cd0f26d89e472
maggik2048 <maggik2048@gmail.com>
2026-02-09 01:04:02 -0800

    Add Unreal project with Content folder (Characters removed)

---
+++ Source/homework/Private/MovingPlatform.cpp
@@ -0,0 +1,29 @@
+﻿#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+    RootComponent = Mesh;
+}
+
+void AMovingPlatform::BeginPlay()
+{
+    Super::BeginPlay();
+    StartLocation = GetActorLocation();
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    FVector NewLocation = GetActorLocation() + MoveDirection * MoveSpeed * DeltaTime;
+
+    if (FVector::Dist(StartLocation, NewLocation) > MaxRange)
+    {
+        MoveDirection *= -1;
+    }
+
+    SetActorLocation(NewLocation);
+}
---
+++ Source/homework/Public/PlatformSpawner.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlatformSpawner.generated.h"
+
+class ARotatingPlatform;
+class AMovingPlatform;
+
+UCLASS()
+class HOMEWORK_API APlatformSpawner : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    APlatformSpawner();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    UPROPERTY(EditAnywhere, Category = "Spawn")
+    int32 RotatingPlatformCount = 5;
+
+    UPROPERTY(EditAnywhere, Category = "Spawn")
+    int32 MovingPlatformCount = 5;
+
+    UPROPERTY(EditAnywhere, Category = "Spawn")
+    TSubclassOf<ARotatingPlatform> RotatingPlatformClass;
+
+    UPROPERTY(EditAnywhere, Category = "Spawn")
+    TSubclassOf<AMovingPlatform> MovingPlatformClass;
+
+    UPROPERTY(EditAnywhere, Category = "Spawn")
+    FVector SpawnArea = FVector(2000, 2000, 0);
+};
